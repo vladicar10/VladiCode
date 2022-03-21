2cd137ff02faad087cbfa19995dadee1066267f2
Vladimiri <vladicar10@gmail.com>
2022-03-20 21:45:23 -0400

    Python structures

--- formula.py
+++ formula.py
@@ -1,1 +1,35 @@
-Code
+#list 
+
+
+from turtle import st
+
+
+str_list = ['Life', 'Is', 'Beautiful']
+
+print(str_list)
+print(str_list[0])
+
+#Tuples
+
+stock1= ('MSTF',95.00, 92)
+type(stock1)
+
+tup1= ('Tuple','is','and','immutable','list')
+print(tup1[0])
+
+tup1[1:3]
+
+#append remov index
+
+#Dictionary
+my_dict={1:'mesft',2:'IT'}
+
+my_dict2={'name':'Aarav',1:[2,4,10]}
+
+#Modyfying dictionaries
+my_dict3={1:'London',2:'Madrid'}
+
+my_dict3[2]='Barcelona'
+
+print(my_dict3)
+
